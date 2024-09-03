af7be5f0acbfe84038179c9e0b68d21a06c3b0ef
yangwu <yangwu@dengtacj.com>
2024-09-03 18:58:27 +0800

    feat：我的样式

--- pages/login-demo/login-demo.vue
+++ pages/login-demo/login-demo.vue
@@ -11,6 +11,8 @@
 		<button type="primary" @click="toPay">paypal1</button>
 		<view>payInfo 调起支付参数: {{payInfo}}</view>
 		<view>captureInfo 付款成功结果: {{captureInfo}}</view>
+
+		<button type="primary" @click="toGooglePay">谷歌支付</button>
 	</view>
 </template>
 
@@ -25,7 +27,7 @@
 				err: '',
 				userInfo: '',
 				payInfo: '',
-				captureInfo:''
+				captureInfo: ''
 			}
 		},
 		methods: {
@@ -84,7 +86,7 @@
 				const data = await PayOrder.createBusinessOrder({
 					amount: 15
 				});
-				
+
 				this.payInfo = data;
 				uni.requestPayment({
 					"provider": "paypal",
@@ -93,15 +95,70 @@
 						var rawdata = JSON.parse(res.rawdata);
 						console.log("orderId：" + rawdata.orderId);
 						let captureInfo = await Paypal.captureOrder({
-							orderId:rawdata.orderId
+							orderId: rawdata.orderId
 						})
-						
+
 						this.captureInfo = captureInfo
 					},
 					fail: function(err) {
 						console.log('fail:' + JSON.stringify(err));
 					}
 				});
+			},
+
+			// 
+			toGooglePay() {
+				let cardPaymentMethodConfig = {
+					environment: 3, // 必填 1 是product  3是test
+					paymentMethodType: "CARD", //必填 CARD、PAYPAL
+					existingPaymentMethodRequired: false, //可选 如果设置为true同时已经准备好了支付allowedPaymentMethods中的付款方式，isReadyToPay就会返回true。
+
+					currencyCode: "USD", //必填
+					countryCode: "US", //在欧洲经济区必填
+					transactionId: "", //当你想要接收googlepay回调的时候必填
+					totalPriceStatus: "FINAL", //必填  NOT_CURRENTLY_KNOWN、ESTIMATED、FINAL
+					totalPrice: "111.00", //必填 满足正则格式^[0-9]+(\.[0-9][0-9])?$
+					totalPriceLabel: "100heelo", //可选
+					checkoutOption: "DEFAULT", //可选 DEFAULT、COMPLETE_IMMEDIATE_PURCHASE
+
+					merchantName: "Example Merchant", //可选
+					emailRequired: true, //可选
+					shippingAddressRequired: true, //可选
+					shippingPhoneNumberRequired: false, //可选
+					allowedCountryCodes: ["US", "GB"], //可选
+					allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"], //必填
+					allowedCardNetworks: ["AMEX", "DISCOVER", "JCB", "MASTERCARD", "VISA"], //必填
+					allowPrepaidCards: false, //可选
+					// allowCreditCards:false,//可选  
+					assuranceDetailsRequired: false, //可选
+					billingAddressRequired: true, //可选
+					billingAddressParametersFormat: "FULL", //可选 MIN
+					phoneNumberRequired: false, //可选
+					tokenizationSpecificationType: "PAYMENT_GATEWAY", //必填 PAYMENT_GATEWAY、DIRECT
+					gateway: "example", //PAYMENT_GATEWAY时必填
+					gatewayMerchantId: "exampleGatewayMerchantId", //PAYMENT_GATEWAY时必填
+					protocolVersion: "", //DIRECT时必填
+					publicKey: "", //DIRECT时必填
+					buildTokenizationSpecification: { //可选，此字段是为了方便开发者自定义构造tokenizationSpecification参数,设置此字段时，会覆盖掉`tokenizationSpecificationType`、`gateway`、`gatewayMerchantId`、`protocolVersion`、`publicKey`字段。(HBuilderX 3.5.1+支持)
+						"type": "PAYMENT_GATEWAY",
+						"parameters": {
+							"gateway": "custom-gateway",
+							"gatewayMerchantId": "mock-gatewayMerchantId"
+						}
+					}
+				};
+
+
+				uni.requestPayment({
+					"provider": "google-pay",
+					"orderInfo": cardPaymentMethodConfig,
+					success: async function(res) {
+						console.log("谷歌支付：", res);
+					},
+					fail: function(err) {
+						console.log('fail:' + JSON.stringify(err));
+					}
+				})
 			}
 		}
 	}
--- pages/user-center/user-center.vue
+++ pages/user-center/user-center.vue
@@ -1,25 +1,74 @@
 <template>
-	<view>
-		<view>用户信息</view>
+	<view class="user-center-page">
+		<!-- 登录状态 -->
+		<view class="login-status-box">
+			<img src="" class="head-img" />
+			Login in
+			<i></i>
+		</view>
+
+		<view class="vip-box">
+			<view class="vip-left-box">
+				<view class="vip-title">Open Storynow Membership</view>
+				<view class="vip-desc">Access to popular content across all platforms.</view>
+			</view>
+			<view class="vip-right">
+				<text>0.3</text>$/day
+			</view>
+		</view>
+
+		<view class="history">
+			<view class="menu-item">
+				<view class="menu-item-icon"></view>
+				<view class="menu-item-title">历史记录</view>
+				<view class="menu-item-more"></view>
+			</view>
+			
+			<view class="read-continue">
+					<text>Stepford Wives Literature's Overbearing President Loves Meg husband gets punished!
+</text>
+					<view class="continue">continue</view>
+			</view>
+		</view>
+
+		<view class="menu-box">
+			<view class="menu-item">
+				<view class="menu-item-icon"></view>
+				<view class="menu-item-title">标题</view>
+				<view class="menu-item-more"></view>
+			</view>
+			<view class="menu-item">
+				<view class="menu-item-icon"></view>
+				<view class="menu-item-title">标题2</view>
+				<view class="menu-item-more"></view>
+			</view>
+		</view>
+
+
+		<!-- 	<view>用户信息</view>
 		<view>{{JSON.stringify(userInfo, null, 2)}}</view>
 		<button v-if="!userInfo._id" @click="goto('/uni_modules/uni-id-pages/pages/login/login-withpwd')">登录</button>
 		<button v-if="!userInfo._id" @click="goto('/uni_modules/uni-id-pages/pages/register/register')">注册</button>
 		<button v-if="userInfo._id" @click="goto('/uni_modules/uni-id-pages/pages/userinfo/change_pwd/change_pwd')">修改密码</button>
 		<button v-if="userInfo._id" @click="logout">退出登录</button>
-		<button v-if="userInfo._id" @click="goto('/uni_modules/uni-id-pages/pages/userinfo/deactivate/deactivate')">注销</button>
+		<button v-if="userInfo._id" @click="goto('/uni_modules/uni-id-pages/pages/userinfo/deactivate/deactivate')">注销</button> -->
 	</view>
 </template>
 
 <script>
 	import {
-	  store,
-	  mutations
-	} from '@/uni_modules/uni-id-pages/common/store.js'
-	
+		store,
+		mutations
+	} from "@/uni_modules/uni-id-pages/common/store.js";
+
 	export default {
 		data() {
 			return {
-				userInfo: store.userInfo
+				userInfo: store.userInfo,
+
+				menuList: [{
+
+				}]
 			};
 		},
 		methods: {
@@ -31,11 +80,187 @@
 			logout() {
 				mutations.logout();
 			},
-			
-		}
-	}
+		},
+	};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+	.user-center-page {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		border: 1px solid red;
 
+		.login-status-box {
+			width: 100%;
+			height: 44px;
+			border: 1px solid black;
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+			margin: 24px;
+			font-family: "Open Sans";
+			font-size: 20px;
+			font-style: normal;
+			font-weight: 600;
+			line-height: 28px;
+
+			.head-img {
+				width: 44px;
+				height: 44px;
+				border-radius: 50%;
+				border: 1px solid red;
+				margin-right: 14px;
+			}
+
+			i {
+				width: 16px;
+				height: 16px;
+				border: 1px solid red;
+				margin-left: 6px;
+			}
+		}
+
+		.vip-box {
+			border: 1px solid red;
+			margin: 0 24px;
+			padding: 12px 16px;
+			background: #FCEDCF;
+			border: 1px solid white;
+			border-radius: 12px 12px 0 0;
+			display: flex;
+			align-items: center;
+			justify-content: space-around;
+
+			.vip-left-box {
+				display: flex;
+				flex-direction: column;
+
+				.vip-title {
+					color: #483510;
+					font-family: "PingFang SC";
+					font-size: 14px;
+					font-style: normal;
+					font-weight: 500;
+					line-height: 20px;
+					/* 142.857% */
+				}
+
+				.vip-desc {
+					color: #A68332;
+					font-family: "Open Sans";
+					font-size: 12px;
+					font-style: normal;
+					font-weight: 400;
+					line-height: 16px;
+					/* 133.333% */
+				}
+			}
+
+			.vip-right {
+				width: 80px;
+				height: 36px;
+				padding: 8px 20px;
+				border-radius: 40px;
+				background: #F8D177;
+				margin-left: 16px;
+				box-sizing: border-box;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				color: #483510;
+				font-family: "Open Sans";
+				font-size: 12px;
+				font-style: normal;
+				font-weight: 400;
+
+				text {
+					color: #483510;
+					font-family: Roboto;
+					font-size: 14px;
+					font-style: normal;
+					font-weight: 600;
+				}
+			}
+		}
+
+		.history {
+			margin: 0 16px;
+			border-radius: 12px;
+			overflow: hidden;
+			margin-bottom: 12px;
+			background: white;
+			.read-continue{
+				margin:0 12px;
+				border-radius: 12px;
+				padding: 12px;
+				margin-bottom: 16px;
+				display: flex;
+				align-items: center;
+				background: var(--light-brand-03, #F0EDFF);
+				text{
+					overflow: hidden;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+				}
+				.continue{
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					display: flex;
+					height: 28px;
+					padding: 8px 12px;
+					justify-content: center;
+					align-items: center;
+					border-radius: 20px;
+					background: rgba(255, 255, 255, 0.60);
+					color: #6B4CFF;
+					font-family: "PingFang SC";
+					font-size: 12px;
+					font-style: normal;
+					font-weight: 500;
+					line-height: 14px; /* 116.667% */
+				}
+			}
+		}
+
+		.menu-box {
+			margin: 0 16px;
+			border-radius: 12px;
+			overflow: hidden;
+		}
+
+		.menu-item {
+			background: #FFF;
+			padding: 16px;
+			display: flex;
+			align-items: center;
+
+			.menu-item-icon {
+				border: 1px solid red;
+				width: 20px;
+				height: 20px;
+				margin-right: 12px;
+			}
+
+			.menu-item-title {
+				flex: 1;
+				display: flex;
+				align-items: center;
+				color: #221F33;
+				font-family: "PingFang SC";
+				font-size: 14px;
+				font-style: normal;
+				font-weight: 500;
+				line-height: 20px;
+				/* 142.857% */
+			}
+
+			.menu-item-more {
+				width: 16px;
+				height: 16px;
+				border: 1px solid red;
+			}
+		}
+	}
 </style>
